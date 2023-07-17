117b6052a265bd1318a7dfa0c540f7b494c4de54
RCSZ <85025880+rcszc@users.noreply.github.com>
2023-07-18 04:38:40 +0800

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,155 @@
+﻿// pomelo_nodeeditor.
+/*
+* by RCSZ [2023_07_15] version 1.0
+* visual studio 2019 MSVC170 x64
+* Libray: ImGui ImNodes GLFW RapidJSON
+* Device: i7-13700HX DDR5 16GiB 4800MHz
+*/
+
+/*
+* [2023.07.15] startup project.
+*/
+
+/* 2019 配置路径.
+* external_library/graphics_library/glfw_libray/include/
+* external_library/graphics_library/glew_libray/include/
+* external_library/guiimgui_library/imgui_glfw/
+* external_library/imnodes_master/
+* external_library/RapidJSON/
+* external_library/graphics_library/glfw_libray/lib-vc2019/
+* external_library/graphics_library/glew_libray/lib/Release/x64/
+* 忽略: msvcrt.lib
+*/
+
+#include <iostream>
+#include <windows.h>
+
+#include <GL/glew.h>
+#include <GL/GL.h>
+#include <GL/GLU.h>
+#include <GLFW/glfw3.h>
+
+#include "pomelo_node_editor/pomelo_node_editor.h"
+
+using namespace std;
+
+namespace DemoGlobal {
+
+    bool             WindowNoClose = false;
+    ImGuiWindowFlags WindowFlags = NULL;
+}
+
+/*
+* Pomelo_NodeEditor Test RenderFunc.
+* GLFW 3.0 OpenGL 460 shader.
+*/
+void GLFW_TEST_EVENTLOOP() { // 循环.
+
+    // NodeEditorGui 绘制标志.
+    static bool drawflag = true;
+
+    PNErender::rendertick_gui(drawflag, []() {
+
+        PNEpreset::component_guipanel();
+    });
+}
+
+void GLFW_TEST_EVENTINIT(GLFWwindow* WinObj) { // 初始化.
+    
+    PNEinit::initialization_imgui(
+        PNEinit::imguiinit(
+            "#version 460",                                                // OpenGL 着色器版本.
+            "external_library/guiimgui_library/imgui_fonts/MSYH_Bold.ttf", // ImGui  字体路径.
+            18.5f,                                                         // ImGui  字体大小.
+            STYLE_TYPE_CYAN                                                // 颜色主题(基于 ImGui.Dark)
+            ), 
+        WinObj
+    );
+
+    PNEinit::initialization_imnodes();
+
+    // 加载组件配置.
+    PNEpreset::component_load("pomelo_node_editor/components_json/demo_cmp_ass1.json");
+    PNEpreset::component_load("pomelo_node_editor/components_json/demo_cmp_ass2.json");
+    PNEpreset::component_load("pomelo_node_editor/components_json/demo_cmp_ass3.json");
+    PNEpreset::component_load("pomelo_node_editor/components_json/demo_cmp_ass4.json");
+}
+
+// ************************************************ GLFW Render Test ************************************************
+// 推!
+// 推!
+// 推!
+// 推!
+// 推!
+// 推!
+// 推!
+// 推!
+
+static void GLFW_ErrorCallback(int error, const char* description) {
+    // GlFW Callback ErrorPrint.
+    fprintf(stderr, "[GLfwSystem]: GLFW Error %d: %s\n", error, description);
+}
+
+void oglwindow_eventloop(GLFWwindow* winobj) {
+    int buffersize[2] = {};
+
+    while (true) {
+        glfwPollEvents();
+        // glfwSetWindowShouldClose(_MainWinodwObj, PSA_TRUE);
+
+        glfwGetFramebufferSize(winobj, &buffersize[0], &buffersize[1]);
+        glViewport(0, 0, buffersize[0], buffersize[1]);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // main => oglwindow_eventloop => render_loop_event
+        GLFW_TEST_EVENTLOOP();
+
+        glfwMakeContextCurrent(winobj);
+        glfwSwapBuffers(winobj);
+    }
+}
+
+int main() {
+    // glfw init create.
+    glfwSetErrorCallback(GLFW_ErrorCallback);
+    if (!glfwInit()) {
+        cout << "GLFW ObjectInit Failed." << endl;
+    }
+
+    // OpenGL Window Config.
+    {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE); // 兼容模式.
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);   // glfw.version 3.2+ [profile].
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);             // glfw.version 3.0+ 
+        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); // Fixed window size.
+        glfwWindowHint(GLFW_SAMPLES, 4);          // 4x Samples MSAA.
+    }
+
+    // create window.
+    GLFWmonitor* CreateFlag = {};
+    GLFWwindow* WindowObject = {};
+
+    WindowObject = glfwCreateWindow(
+        1440, 810,
+        "Pomelo NodeEditor", CreateFlag, nullptr
+    );
+
+    // create context.
+    glfwMakeContextCurrent(WindowObject);
+    // Enable Async. 开启垂直同步.
+    glfwSwapInterval(1);
+
+    // Init.
+    GLFW_TEST_EVENTINIT(WindowObject);
+
+    // main => oglwindow_eventloop
+    oglwindow_eventloop(WindowObject);
+
+    // opengl free window.
+    glfwDestroyWindow(WindowObject);
+    glfwTerminate();
+}
---
+++ node_editor_src/pomelo_node_editor_cmp.cpp
@@ -0,0 +1,344 @@
+// pomelo_node_editor_cmp.
+#include "pomelo_node_editor.h"
+
+using namespace std;
+
+vector<pne_node_component::node_attribute> __NODE_COMPONENTS = {};
+vector<pne_node_component::node_attribute> __NODE_COMPONENTS_DCS = {};
+// Link management HashMap.
+unordered_map<int32_t, pne_node_component::node_link_line> __NODE_LINESLINK = {};
+
+/*
+* @param  ImVec4 (in Color, Ahpla += w)
+* @return ImVec4
+*/
+ImVec4 ColorAhplaAdd(ImVec4 color, float w) {
+    ImVec4 rteurn_color = ImVec4(
+        color.x,
+        color.y,
+        color.z,
+        color.w + w
+    );
+    return rteurn_color;
+}
+
+void PNEGET_node_link_data(
+    std::vector<pne_node_component::node_attribute>&                 node_data,
+    std::unordered_map<int32_t, pne_node_component::node_link_line>& link_data
+) {
+    node_data = __NODE_COMPONENTS_DCS;
+    link_data = __NODE_LINESLINK;
+}
+
+namespace pne_node_component {
+    // 连接管理系统.
+    namespace LINKSYSTEM {
+
+        void component_link_lines(
+            vector<pne_node_component::node_attribute>& nodes,
+            unordered_map<int32_t, pne_node_component::node_link_line>& linklines
+        ) {
+            // LINK unique identification.
+            static uint32_t link_count = 1;
+
+            node_link_line create_link = {};
+            PARAMTYPE      type_temp[2] = {};
+
+            // IsLink => LinkLines.
+            if (ImNodes::IsLinkCreated(&create_link.begin_point, &create_link.end_point)) {
+
+                for (size_t i = 0; i < nodes.size(); ++i) {
+                    for (size_t j = 0; j < nodes[i].in_connect.size(); ++j) {
+
+                        // input 连接点所在 node => 互联标识(line_id).
+                        if ((nodes[i].in_connect[j].point_number == create_link.begin_point) || 
+                            (nodes[i].in_connect[j].point_number == create_link.end_point)) 
+                        {
+                            nodes[i].in_connect[j].linkline_number.push_back(link_count);
+                            type_temp[0] = nodes[i].in_connect[j].point_param_type;
+
+                            create_link.link_line_color = nodes[i].in_connect[j].point_color;
+                        }
+                    }
+                    for (size_t j = 0; j < nodes[i].out_connect.size(); ++j) {
+
+                        // output 连接点所在 node => 互联标识(line_id).
+                        if ((nodes[i].out_connect[j].point_number == create_link.begin_point) ||
+                            (nodes[i].out_connect[j].point_number == create_link.end_point))
+                        {
+                            nodes[i].out_connect[j].linkline_number.push_back(link_count);
+                            type_temp[1] = nodes[i].out_connect[j].point_param_type;
+                        }
+                    }
+                }
+
+                // 类型相同可连接.
+                if (type_temp[0] == type_temp[1]) {
+
+                    // IsLink => create push.
+                    linklines.insert(make_pair(link_count, create_link));
+                    link_count += CREATE_UNIQUE_STEP;
+                }
+            }
+        }
+
+        void component_link_break(
+            unordered_map<int32_t, pne_node_component::node_link_line>& linklines,
+            int linkline_id
+        ) {
+            // 断开连接线, Erase hash_map value.
+            linklines.erase(linkline_id);
+        }
+
+        void component_node_delete(
+            vector<pne_node_component::node_attribute>& nodes,
+            int nodes_id
+        ) {
+            // 删除节点, (搜索连线 => 删除节点所有连线 => 删除节点)
+            // O(n) search node_id.
+            for (size_t i = 0; i < nodes.size(); ++i) {
+                if (nodes[i].node_number == nodes_id) {
+                    for (size_t j = 0; j < nodes[i].in_connect.size(); ++j) {
+
+                        // input 删除相关互联.
+                        for (size_t d = 0; d < nodes[i].in_connect[j].linkline_number.size(); ++d) {
+                            component_link_break(
+                                __NODE_LINESLINK,
+                                nodes[i].in_connect[j].linkline_number[d]
+                            );
+                        }
+                    }
+                    for (size_t j = 0; j < nodes[i].out_connect.size(); ++j) {
+
+                        // output 删除相关互联.
+                        for (size_t d = 0; d < nodes[i].out_connect[j].linkline_number.size(); ++d) {
+                            component_link_break(
+                                __NODE_LINESLINK,
+                                nodes[i].out_connect[j].linkline_number[d]
+                            );
+                        }
+                    }
+                    // delete node.
+                    nodes.erase(nodes.begin() + i);
+                }
+            }
+        }
+
+        void component_node_menu() {
+
+            static bool window_create  = false;
+            static bool window_hovered = false;
+
+            static int break_linkline_id = -1;
+            static int delete_node_id    = -1;
+
+            // open close menu.
+            if ((ImGui::IsMouseClicked(1) == true) && (window_create == false)) {
+                // 创建窗口为鼠标位置.
+                ImGui::SetNextWindowPos(ImGui::GetMousePos());
+
+                ImNodes::IsLinkHovered(&break_linkline_id);
+                ImNodes::IsNodeHovered(&delete_node_id);
+
+                window_create = true;
+            }
+            else { 
+                // 点击任意键菜单栏关闭.
+                if (((ImGui::IsMouseClicked(0) == true) || (ImGui::IsMouseClicked(1) == true))
+                    && (window_create == true)
+                    && (window_hovered == false))
+                {
+                    window_create  = false;
+                    window_hovered = false;
+
+                    break_linkline_id = -1;
+                    delete_node_id    = -1;
+                }
+            }
+
+            if (window_create) {
+                ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 7.2f); // rounded window.
+                ImGui::SetNextWindowSize(ImVec2(128.0f, 192.0f));
+
+                ImGui::Begin("Pure Window", nullptr,
+                    ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+
+                ImGui::Text(u8"节点菜单");
+                if (ImGui::Button(u8"断开连接", ImVec2(112.0f, 32.0f)) 
+                    && break_linkline_id > 0
+                ) {
+                    component_link_break(__NODE_LINESLINK, break_linkline_id);
+
+                    window_create  = false;
+                    window_hovered = false;
+                }
+
+                if (ImGui::Button(u8"删除节点", ImVec2(112.0f, 32.0f))
+                    && delete_node_id > 0
+                ) {
+                    component_node_delete(__NODE_COMPONENTS_DCS, delete_node_id);
+                   
+                    window_create  = false;
+                    window_hovered = false;
+                }
+                
+                window_hovered = ImGui::IsWindowHovered();
+                ImGui::End();
+                ImGui::PopStyleVar();
+            }
+        }
+    }
+
+    void component_pcs_tick() {
+
+        component_render(__NODE_COMPONENTS_DCS, __NODE_LINESLINK);
+
+        LINKSYSTEM::component_link_lines(__NODE_COMPONENTS_DCS, __NODE_LINESLINK);
+        LINKSYSTEM::component_node_menu();
+    }
+
+#define SRAND_NODE_POSMIN -10000
+#define SRAND_NODE_POSMAX 10000
+
+    void component_allocation(node_attribute& node) {
+        // nodes & connect unique identification.
+        static uint32_t node_count = 1;
+        static uint32_t connectpoint_count = 1;
+        
+        // 随机 node 创建位置.
+        float srandpos[2] = {
+            float(rand() % (SRAND_NODE_POSMAX - SRAND_NODE_POSMIN + 1) + SRAND_NODE_POSMIN) / 100.0f,
+            float(rand() % (SRAND_NODE_POSMAX - SRAND_NODE_POSMIN + 1) + SRAND_NODE_POSMIN) / 100.0f
+        };
+
+        node.node_number = node_count;
+        // set create pos. [srand]
+        ImNodes::SetNodeGridSpacePos(
+            node_count, 
+            ImVec2(320.0f + srandpos[0], 320.0f + srandpos[1])
+        );
+
+        node_count += CREATE_UNIQUE_STEP;
+
+        for (size_t i = 0; i < node.in_connect.size(); ++i) {
+            // input connectpoint count.
+            node.in_connect[i].point_number = connectpoint_count;
+            connectpoint_count += CREATE_UNIQUE_STEP;
+        }
+
+        for (size_t i = 0; i < node.out_connect.size(); ++i) {
+            // output connectpoint count.
+            node.out_connect[i].point_number = connectpoint_count;
+            connectpoint_count += CREATE_UNIQUE_STEP;
+        }
+    }
+
+    // editor window fixed. 
+    bool editor_fixed_move = true;
+
+	void component_render(
+        vector<node_attribute>& dwnodes,
+        unordered_map<int32_t, pne_node_component::node_link_line>& lines
+    ) {
+        static bool             editor_no_close = false;
+        static ImGuiWindowFlags editor_flags = NULL;
+
+        if (editor_fixed_move) {
+            editor_flags = ImGuiWindowFlags_NoMove;    // 固定位置.
+            editor_flags |= ImGuiWindowFlags_NoResize; // 固定大小.
+            editor_flags |= ImGuiWindowFlags_NoCollapse;
+        }
+        else
+            editor_flags = NULL;
+
+        ImGui::Begin("Pomelo NodesEditor", &editor_no_close, editor_flags);
+        ImNodes::BeginNodeEditor();
+        // 节点编辑器.
+        {
+            // ergodic vector<node_attribute>.
+            for (const auto& dwnode : dwnodes) {
+
+                ImNodes::PushColorStyle( // 常态颜色. [NodeBar]
+                    ImNodesCol_TitleBar,
+                    ImGui::ColorConvertFloat4ToU32(ImVec4(dwnode.node_color))
+                );
+                ImNodes::PushColorStyle( // 悬停颜色. [NodeBar]
+                    ImNodesCol_TitleBarHovered,
+                    ImGui::ColorConvertFloat4ToU32(ImVec4(dwnode.node_color_click))
+                );
+                ImNodes::PushColorStyle( // 点击颜色. [NodeBar]
+                    ImNodesCol_TitleBarSelected,
+                    ImGui::ColorConvertFloat4ToU32(ImVec4(dwnode.node_color_click))
+                );
+                
+                ImNodes::BeginNode(dwnode.node_number);
+                // 节点积木.
+                {
+                    ImNodes::BeginNodeTitleBar();
+                    ImGui::TextUnformatted(dwnode.node_title.c_str());
+                    ImNodes::EndNodeTitleBar();
+
+                    for (size_t i = 0; i < dwnode.in_connect.size(); ++i) {
+
+                        // 输入 连接点.
+                        ImNodes::BeginInputAttribute(dwnode.in_connect[i].point_number);
+                        
+                        ImGui::TextColored(
+                            dwnode.in_connect[i].point_color, 
+                            dwnode.in_connect[i].point_text.c_str()
+                        );
+
+                        ImNodes::EndInputAttribute();
+                    }
+
+                    for (size_t i = 0; i < dwnode.out_connect.size(); ++i) {
+
+                        // 输出 连接点.
+                        ImNodes::BeginOutputAttribute(dwnode.out_connect[i].point_number);
+                        ImGui::Indent(50.0f);
+
+                        ImGui::TextColored(
+                            dwnode.out_connect[i].point_color,
+                            dwnode.out_connect[i].point_text.c_str()
+                        );
+                        ImNodes::EndOutputAttribute();
+                    }
+                }
+                ImNodes::EndNode();
+
+                ImNodes::PopColorStyle();
+                ImNodes::PopColorStyle();
+            }
+            
+            for (const auto& link : lines) {
+                // link lines color = connect.point_color.
+                // static.
+                ImNodes::PushColorStyle(
+                    ImNodesCol_Link,
+                    ImGui::ColorConvertFloat4ToU32(link.second.link_line_color)
+                );
+                // hovered.
+                ImNodes::PushColorStyle(
+                    ImNodesCol_LinkHovered,
+                    ImGui::ColorConvertFloat4ToU32(
+                        ColorAhplaAdd(link.second.link_line_color, -0.42f)
+                ));
+                // selected.
+                ImNodes::PushColorStyle(
+                    ImNodesCol_LinkSelected,
+                    ImGui::ColorConvertFloat4ToU32(
+                        ColorAhplaAdd(link.second.link_line_color, -0.42f)
+                ));
+
+                // link connect point.
+                ImNodes::Link(link.first, link.second.begin_point, link.second.end_point);
+
+                ImNodes::PopColorStyle();
+                ImNodes::PopColorStyle();
+                ImNodes::PopColorStyle();
+            }
+        }
+        ImNodes::EndNodeEditor();
+        ImGui::End();
+	}
+}
